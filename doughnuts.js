3bfdf58a6fb257fee01e1e229a660640e4c1a0a8
creamydesire <70323654+creamydesire@users.noreply.github.com>
2020-08-29 15:20:37 +0530

    Update doughnuts.js

--- doughnuts.js
+++ doughnuts.js
@@ -1,9 +1,9 @@
-$(".mouseover").on("mouseover",function(){
+$(".mouseover").on("mouseenter",function(){
 
 	$(this).css("cursor", "pointer"); 
 	$(this).animate({"width":"30em","height":"30em"},"fast");
 });
 
-$(".mouseover").on("mouseout",function(){
+$(".mouseover").on("mouseleave",function(){
 	$(this).animate({"width":"27em", "height":"22em"},"fast");
 });
